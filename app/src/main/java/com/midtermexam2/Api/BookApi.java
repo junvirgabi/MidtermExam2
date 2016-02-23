c69d3eaeafba0fa137b8eebb5823746665b65421
junvirgabi <junvir12@gmail.com>
2016-02-23 17:24:38 +0800

    added book api and entity: book with Improved Listview

--- app/src/main/java/com/midtermexam2/Api/BookApi.java
+++ app/src/main/java/com/midtermexam2/Api/BookApi.java
@@ -3,13 +3,50 @@ package com.midtermexam2.Api;
 /**
  * Created by junvir on 2/23/2016.
  */
+
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+
+import com.midtermexam2.Book;
+import com.midtermexam2.Utils.HttpUtils;
+
+import org.json.JSONObject;
+
+import java.util.List;
+
+
 public class BookApi {
 
     public static final String API_URL ="http://172.17.3.51:5896/api/books";
-    public static final String OB_ID = "_id";
-    public static final String OB_TITLE = "title";
-    public static final String OB_GENRE = "genre";
-    public static final String OB_AUTHOR= "author";
-    public static final String OB_ISREAD = "isread";
+    public static final String BOOK_ID = "_id";
+    public static final String BOOK_TITLE = "title";
+    public static final String BOOK_GENRE = "genre";
+    public static final String BOOK_AUTHOR= "author";
+    public static final String BOOK_ISREAD = "isread";
+
+
+    public static Book getId(Uri uri, @NonNull String requestMethod ){
+
+        List <Book> books;
+        String json = HttpUtils.getResponse(uri, requestMethod);
+
+        if (TextUtils.isEmpty(json)) {
+            return null;
+        }
+
+            return null;
+
+
+    }
+
+
+    JSONObject json = new JSONObject();
+    Book book = new Book("56c9c4f06d39c541658293d9","not read", "Historical Fiction", "War and Peace","Lev Nikolayevich Tolstoy" );
+
+
+
 
 }
--- app/src/main/java/com/midtermexam2/MainActivity.java
+++ app/src/main/java/com/midtermexam2/MainActivity.java
@@ -29,22 +29,25 @@ public class MainActivity extends AppCompatActivity {
         // Find the ListView resource.
         mainListView = (ListView) findViewById( R.id.mainListView );
 
-        // Create and populate a List of planet names.
-        String[] planets = new String[] { "Lev Nikolayevich Tolstoy", "Les Misérables", "The Time Machine", "A Journey into the Center of the Earth",
+        // Create and populate a List of books.
+        String[] planets = new String[] { "War and Peace", "Les Misérables", "The Time Machine", "A Journey into the Center of the Earth",
                 "The Dark World", "The Wind in the Willows", "Life On The Mississippi", "Childhood"};
-        ArrayList<String> planetList = new ArrayList<String>();
-        planetList.addAll( Arrays.asList(planets) );
+        ArrayList<String> bookList = new ArrayList<String>();
+        bookList.addAll( Arrays.asList(planets) );
 
-        // Create ArrayAdapter using the planet list.
-        listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, planetList);
+        // Create ArrayAdapter using the books list.
+        listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, bookList);
 
-        // Add more planets. If you passed a String[] instead of a List<String>
-        // into the ArrayAdapter constructor, you must not add more items.
-        // Otherwise an exception will occur.
 
-        // Set the ArrayAdapter as the ListView's adapter.
         mainListView.setAdapter( listAdapter );
 
-        mainListView.setOnItemClickListener((AdapterView.OnItemClickListener) this);
+
     }
+
+//    @Override
+//    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//        Intent intent = new Intent(getActivity(), MovieDetailsActivity.class);
+//        intent.putExtra(Constants.EXTRA_POSITION, position);
+//        startActivity(intent);
+//    }
 }
